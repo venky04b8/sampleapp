973e3c727e06a038ff030ec143a61bd9a24aa8f7
venky04b8 <venkyuvr@gmail.com>
2018-03-10 11:58:46 +0530

    In app browsing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { CallNumber } from '@ionic-native/call-number';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -26,6 +27,7 @@ import { HomePage } from '../pages/home/home';
     StatusBar,
     SplashScreen,
     CallNumber,
+    InAppBrowser,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import {Component, enableProdMode} from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { CallNumber } from '@ionic-native/call-number';
+import {InAppBrowser, InAppBrowserOptions} from '@ionic-native/in-app-browser';
 
 enableProdMode();
 @Component({
@@ -9,10 +10,27 @@ enableProdMode();
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController,private callNumber: CallNumber) {
+  constructor(public navCtrl: NavController,private callNumber: CallNumber,private theInAppBrowser: InAppBrowser) {
 
   }
 
+  options : InAppBrowserOptions = {
+    location : 'yes',//Or 'no'
+    hidden : 'no', //Or  'yes'
+    clearcache : 'yes',
+    clearsessioncache : 'yes',
+    zoom : 'yes',//Android only ,shows browser zoom controls
+    hardwareback : 'yes',
+    mediaPlaybackRequiresUserAction : 'no',
+    shouldPauseOnSuspend : 'no', //Android only
+    closebuttoncaption : 'Close', //iOS only
+    disallowoverscroll : 'no', //iOS only
+    toolbar : 'yes', //iOS only
+    enableViewportScale : 'no', //iOS only
+    allowInlineMediaPlayback : 'no',//iOS only
+    presentationstyle : 'pagesheet',//iOS only
+    fullscreen : 'yes',//Windows only
+  };
   launchDialer(n:string){
     // this.callNumber.callNumber(n, true)
     //     .then(() => console.log('Launched dialer!'))
@@ -20,6 +38,18 @@ export class HomePage {
     setTimeout(() => {
       window.open(`tel:${n}`, '_system');
     },100);
+  }
+  public openWithInAppBrowser(url : string){
+    let target = "_blank";
+    this.theInAppBrowser.create(url,target,this.options);
+  }
+  public openWithSystemBrowser(url : string){
+    let target = "_system";
+    this.theInAppBrowser.create(url,target,this.options);
+  }
+  public openWithCordovaBrowser(url : string){
+    let target = "_self";
+    this.theInAppBrowser.create(url,target,this.options);
   }
 
 }
